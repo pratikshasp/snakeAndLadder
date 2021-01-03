32e601bb02e2d0666aff6af1ad3d74ebbe36a96b
Pratiksha Parsewar <pratiksha.parsewar20@gmail.com>
2021-01-03 16:18:05 +0530

    Initial Setup

---
+++ src/main/java/com/snakeAndLadder/SnakeAndLadderApplication.java
@@ -0,0 +1,12 @@
+package com.snakeAndLadder;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeAndLadderApplication {
+
+    public static void main(String[] args) {
+        //TO-DO: solution
+    }
+
+}
---
+++ src/test/java/com/snakeAndLadder/SnakeAndLadderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snakeAndLadder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeAndLadderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
